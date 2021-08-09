f33132565667a77e8c402c97bd65daefdf646021
eros <eros.maurilio@gmail.com>
2021-08-09 18:57:23 -0300

    Adiciona alterações do primeiro dia de projeto

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    private var students = ["Eros", "Ana", "Erick", "Djeni"]
+    @State private var selectedStudent = "Eros"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                Text("Hello, world!")
+                    .padding()
+                }
+                
+                Section {
+                    Button("Tap Count \(tapCount)") {
+                        self.tapCount += 1
+                    }
+                }
+                
+                Section {
+                    TextField("Enter you name:", text: $name)
+                    Text("Your name is \(name)")
+                    
+                }
+                
+                Section {
+                    ForEach(0..<students.count) {
+                        Text(self.students[$0])
+                        
+                    }
+                }
+                
+                Section {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        ForEach(0..<students.count) {
+                            Text(self.students[$0])
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("SwiftUI"), displayMode: .automatic)
+        }
     }
 }
 
