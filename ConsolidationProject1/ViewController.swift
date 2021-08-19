2ace55a70c30ee8c3661fd7f85430bc2071ff369
eros <eros.maurilio@gmail.com>
2021-08-19 13:53:10 -0300

    Initial Commit

---
+++ ConsolidationProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsolidationProject1
+//
+//  Created by Eros Maurilio on 19/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
