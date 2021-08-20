610d7d642d52fe2ba27dc7893ad53e97771c99d8
eros <eros.maurilio@gmail.com>
2021-08-20 14:51:15 -0300

    Corrige bug das células da TableView

--- ConsolidationProject1/ViewController.swift
+++ ConsolidationProject1/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  ConsolidationProject1
-//
-//  Created by Eros Maurilio on 19/08/21.
-//
-
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var cellSpacingHeigh: CGFloat = 15
+    var numberOfRows = 1
+    var identifier = "Flag"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Flag List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        countries = countries.sorted()
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int { countries.count }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { numberOfRows }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { cellSpacingHeigh }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = countries[indexPath.section]
+        cell.imageView?.image = UIImage(named: countries[indexPath.section])
+        cell.imageView?.layer.borderWidth = 1
+        cell.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        
+        return cell
+    }
 }
 
