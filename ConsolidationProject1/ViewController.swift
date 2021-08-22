397252e3759d7dc6e65087960092fd1ba60ce8a5
eros <eros.maurilio@gmail.com>
2021-08-22 12:56:56 -0300

    Resolve Challenge de consolidação 1

--- ConsolidationProject1/ViewController.swift
+++ ConsolidationProject1/ViewController.swift
@@ -37,5 +37,13 @@ class ViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.selectedImage = countries[indexPath.section]
+            vc.imageTitle = countries[indexPath.section]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
