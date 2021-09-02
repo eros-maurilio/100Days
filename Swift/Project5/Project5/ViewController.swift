fccb9fc1216384099838f5009cdc0658c5b36560
eros <eros.maurilio@gmail.com>
2021-09-02 19:09:18 -0300

    Resolve bugs e adiciona alterações projeto 5 dia 28

--- Swift/Project5/Project5/ViewController.swift
+++ Swift/Project5/Project5/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UITableViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAwnser))
         
-        if let startWordsURL = Bundle.main.url(forResource: "Start", withExtension: "txt") {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
             }
@@ -24,6 +24,7 @@ class ViewController: UITableViewController {
         
         if allWords.isEmpty {
         allWords = ["Silkworm "]
+            
         }
         
         startGame()
@@ -36,7 +37,9 @@ class ViewController: UITableViewController {
     }
     
     
-    override func numberOfSections(in tableView: UITableView) -> Int { usedWords.count }
+    override func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { usedWords.count }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
@@ -59,6 +62,61 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossivle(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                    
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            } else {
+                errorTitle = "Word already used"
+                errorMessage = "Be more original!"
+            }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title!.lowercased())."
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func isPossivle(word: String) -> Bool{
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool { !usedWords.contains(word) }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
         
     }
 }
---
+++ SwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Eros Maurilio on 02/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Hello")
+            Text("Hello")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
