5568b6617f3ca204f7592fd4417bb4d7a1d208f6
eros <eros.maurilio@gmail.com>
2021-09-09 15:07:46 -0300

    Resolve desafio projeto 5 dia 29

--- Swift/Project5/Project5/ViewController.swift
+++ Swift/Project5/Project5/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAwnser))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -30,7 +31,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -54,7 +55,7 @@ class ViewController: UITableViewController {
         let submitAction = UIAlertAction(title: "Submit", style: .default) {
             [weak self, weak ac] _ in
             guard let answer = ac?.textFields?[0].text else { return }
-            self?.submit(answer)
+            self?.submit(answer.lowercased())
         }
         
         ac.addAction(submitAction)
@@ -62,14 +63,10 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
-        let lowerAnswer = answer.lowercased()
-        
-        let errorTitle: String
-        let errorMessage: String
-        
-        if isPossivle(word: lowerAnswer) {
-            if isOriginal(word: lowerAnswer) {
-                if isReal(word: lowerAnswer) {
+    
+        if isPossible(word: answer) {
+            if isOriginal(word: answer) {
+                if isReal(word: answer) {
                     usedWords.insert(answer, at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
@@ -78,24 +75,17 @@ class ViewController: UITableViewController {
                     return
                     
                 } else {
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(alertTitle: "Word not recognized", alertMessage: "You can't just make them up, you know!")
                 }
             } else {
-                errorTitle = "Word already used"
-                errorMessage = "Be more original!"
+                showErrorMessage(alertTitle: "Word already used", alertMessage: "Be more original!")
             }
         } else {
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title!.lowercased())."
+            showErrorMessage(alertTitle: "Word not possible", alertMessage: "You can't spell that word from \(title!.lowercased()).")
         }
-        
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Ok", style: .default))
-        present(ac, animated: true)
     }
     
-    func isPossivle(word: String) -> Bool{
+    func isPossible(word: String) -> Bool{
         guard var tempWord = title?.lowercased() else { return false }
         
         for letter in word {
@@ -116,8 +106,16 @@ class ViewController: UITableViewController {
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
-        return misspelledRange.location == NSNotFound
+        let wordArray = Array(word)
         
+        return misspelledRange.location == NSNotFound && wordArray.count >= 3 && word != title
+        
+    }
+    
+    func showErrorMessage(alertTitle: String, alertMessage: String) {
+        let ac = UIAlertController(title: alertMessage, message: alertMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
     }
 }
 
