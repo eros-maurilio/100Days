6707025b9726119f15b2644e48a546752f64de53
Eros Maurilio <eros.maurilio@gmail.com>
2021-09-20 17:16:42 -0300

    Adiciona copia projeto 2(projeto 6a)

---
+++ Swift/Project6a/Project2-100DaysOfSwift/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Project2-100DaysOfSwift
+//
+//  Created by Eros Maurilio on 12/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var totalQuestions = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(scoreTapped))
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "us", "uk"]
+    
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+        
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+
+        title = countries[correctAnswer].uppercased() + " Score: \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            totalQuestions += 1
+        } else {
+            title = "Wrong! That’s the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+            totalQuestions += 1
+        }
+        
+        if totalQuestions < 10 {
+            let ac = alertStyle(title: "\(title) \(totalQuestions) of 10", message: "Your score is \(score)", actionTitle: "Continue")
+            present(ac, animated: true)
+            
+        } else {
+            let fac = alertStyle(title: "Ended!", message: "The game is over, your total score was \(score)", actionTitle: "Restart")
+            present(fac, animated: true)
+            totalQuestions = 0
+            score = 0
+            
+        }
+    }
+    
+    func alertStyle(title: String, message: String, actionTitle: String) -> UIViewController {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: actionTitle, style: .default, handler: askQuestion))
+        
+        return ac
+    }
+    
+    @objc func scoreTapped() {
+        let currentScore = "Your score is: \(score)"
+        let vc = UIActivityViewController(activityItems: [currentScore], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
+
