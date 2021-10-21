a1c4ccc9b7f47b4e8d9b88add1f15c616b2c812d
Eros Maurilio <eros.maurilio@gmail.com>
2021-10-21 19:47:02 -0300

    Altera código dia 37

---
+++ SwiftUI/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Eros Maurilio on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
