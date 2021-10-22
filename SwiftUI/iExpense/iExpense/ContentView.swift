7299da867b2557a79543e5f057ab35b81dab9375
Eros Maurilio <eros.maurilio@gmail.com>
2021-10-21 21:42:53 -0300

    Adiciona alterações dia 37 iExpense

--- SwiftUI/iExpense/iExpense/ContentView.swift
+++ SwiftUI/iExpense/iExpense/ContentView.swift
@@ -7,10 +7,71 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+    
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        
+        self.items = []
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpanse = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List{
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                    
+                }
+                .onDelete(perform: removeItems)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        self.showingAddExpanse = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddExpanse) {
+                AddView(expenses: self.expenses)
+            }
+            .navigationTitle("iExpanse")
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
