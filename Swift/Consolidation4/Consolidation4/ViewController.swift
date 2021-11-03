f3afe43a912746545b9f7556fa44bdb0357c9d48
Eros Maurilio <eros.maurilio@gmail.com>
2021-11-03 19:17:16 -0300

    Cria consolidation4 project

---
+++ Swift/Consolidation4/Consolidation4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Consolidation4
+//
+//  Created by Eros Maurilio on 03/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
