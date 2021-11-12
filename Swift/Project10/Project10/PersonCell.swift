98424a07a748c9cb8f536a02f10a49f84fdc8a3c
Eros Maurilio <eros.maurilio@gmail.com>
2021-11-12 15:23:16 -0300

    Faz alterações dia 42

---
+++ Swift/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Eros Maurilio on 12/11/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var name: UILabel!
+    
+}
