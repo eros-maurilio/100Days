4e2ded169a2b28e0e9fdcf01919315974f68d4a8
Eros Maurilio <eros.maurilio@gmail.com>
2021-11-17 15:13:53 -0300

    Adiciona alterações dia 43

---
+++ Swift/Project10/Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Eros Maurilio on 17/11/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
