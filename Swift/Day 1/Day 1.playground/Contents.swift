9589cf26fb5fd809d50ad0b24ffcfa850350d385
Eros Maurilio <eros.maurilio@gmail.com>
2022-01-11 18:42:27 -0300

    Adiciona playground Day 1

---
+++ Swift/Day 1/Day 1.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+// MARK: - Variables
+
+var greeting = "Hello, playground"
+
+greeting = "GoodBye"
+
+
+// MARK: - Integers
+
+var age = 38
+var population = 8_000_000
+
+// Underscore make easier to read
+
+
+// MARK: - Multi-line Strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This not goes \
+over multiple \
+lines
+"""
+
+
+// MARK: - Doubles and Bools
+
+var pi = Double.pi
+
+var awesome = true
+
+
+// MARK: - String Interpolation
+
+var score = 85
+var str = "Your score was \(score)"
+
+var results = "The test result was: \(str)"
+
+
+// MARK: - Constants
+
+let taylor = "swift"
+
+
+// MARK: Type Annotations
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
